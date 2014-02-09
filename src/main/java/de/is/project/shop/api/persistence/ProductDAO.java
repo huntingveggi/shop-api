cce985485f5784717d06aba92b78a8af3fe806b9
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-02-09 19:40:23 +0100

    Add findBy... methods to ProductDAO interface
    Add new findBy... methods to the interface. The DAO should
    handle the findBy.. methods

--- src/main/java/de/is/project/shop/api/persistence/ProductDAO.java
+++ src/main/java/de/is/project/shop/api/persistence/ProductDAO.java
@@ -3,6 +3,8 @@ package de.is.project.shop.api.persistence;
 import java.util.Collection;
 import java.util.HashMap;
 
+import de.is.project.shop.api.domain.Category;
+import de.is.project.shop.api.domain.Producer;
 import de.is.project.shop.api.domain.Product;
 
 public interface ProductDAO {
@@ -12,6 +14,10 @@ public interface ProductDAO {
 	Product findById(int id);
 
 	Collection<Product> findByAttributes(HashMap<String, Object> map);
+
+	Collection<Product> findByCategories(Collection<Category> categories);
+
+	Collection<Product> findByProducer(Producer producer);
 
 	Product update(Product product);
 

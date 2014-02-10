5785048792acd013e0f2626d76d79409265c9d2d
Thagor <red_steel@web.de>
2014-02-10 19:08:38 +0100

    Updated Interfaces - Methods added

--- src/main/java/de/is/project/shop/api/domain/Attribute.java
+++ src/main/java/de/is/project/shop/api/domain/Attribute.java
@@ -2,4 +2,12 @@ package de.is.project.shop.api.domain;
 
 public interface Attribute {
 
+	public String getName();
+
+	public void setName(String name);
+
+	public String getValue();
+
+	public void setValue(String value);
+
 }

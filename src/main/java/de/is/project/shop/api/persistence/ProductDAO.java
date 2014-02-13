c4ed42f2169f349ff0abf7bd8d0cff134237706e
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-02-13 19:46:36 +0100

    Change ProductDAO - extends CrudDAO

--- src/main/java/de/is/project/shop/api/persistence/ProductDAO.java
+++ src/main/java/de/is/project/shop/api/persistence/ProductDAO.java
@@ -7,7 +7,7 @@ import de.is.project.shop.api.domain.Category;
 import de.is.project.shop.api.domain.Producer;
 import de.is.project.shop.api.domain.Product;
 
-public interface ProductDAO {
+public interface ProductDAO extends CrudDAO<Product> {
 
 	Product persist(Product product);
 

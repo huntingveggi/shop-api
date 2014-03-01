1bcb405f95364c0ad74b8e2fca4186ace6667499
Stefan Stahl <red_steel@web.de>
2014-03-01 13:27:01 +0100

    Add RequestService interface

---
+++ src/main/java/de/is/project/shop/api/services/RequestService.java
@@ -0,0 +1,11 @@
+package de.is.project.shop.api.services;
+
+import de.is.project.shop.api.domain.Request;
+
+public interface RequestService {
+
+	public void setRequest(Request request);
+	
+	public Request updateRequest(Request request);
+	
+}
